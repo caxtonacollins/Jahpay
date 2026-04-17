0f233f0d153c7c1ee8b0bf6054cdad32468f1906
technimite <dev@navin.local>
2026-04-18 00:27:39 +0100

    refactor: update app layout configuration

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -7,6 +7,7 @@ import { WalletProvider } from "@/components/wallet-provider";
 import { Footer } from "@/components/layout/footer";
 import { Toaster } from "@/components/ui/toaster";
 import { TransactionsProvider } from "@/contexts/transactions-context";
+import { MiniPayProvider } from "@/contexts/minipay-context";
 import { WalletRedirect } from "@/components/wallet-redirect";
 import { FooterWrapper } from "@/components/layout/footer-wrapper";
 
@@ -29,15 +30,17 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={inter.className}>
         <div className="relative flex min-h-screen flex-col">
-          <WalletProvider>
-            <TransactionsProvider>
-              <WalletRedirect />
-              <Navbar />
-              <main className="flex-1 pt-20">{children}</main>
-              <FooterWrapper />
-              <Toaster />
-            </TransactionsProvider>
-          </WalletProvider>
+          <MiniPayProvider>
+            <WalletProvider>
+              <TransactionsProvider>
+                <WalletRedirect />
+                <Navbar />
+                <main className="flex-1 pt-20">{children}</main>
+                <FooterWrapper />
+                <Toaster />
+              </TransactionsProvider>
+            </WalletProvider>
+          </MiniPayProvider>
         </div>
       </body>
     </html>
